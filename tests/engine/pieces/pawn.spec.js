0a2580de3405ce7df7ed30274f13d01558013894
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    try commit

--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -1,6 +1,7 @@
 import 'chai/register-should';
 import Pawn from '../../../src/engine/pieces/pawn';
 import Rook from '../../../src/engine/pieces/rook';
+import King from '../../../src/engine/pieces/king';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
@@ -33,6 +34,60 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(2, 7), Square.at(3, 7)]);
         });
 
+<<<<<<< HEAD
+=======
+        it('cannot move at the top of the board', () => {
+            const pawn = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(7, 3), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.be.empty;
+        });
+
+        it('can move diagonally if there is a piece to take', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const opposingPiece = new Rook(Player.BLACK);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(5, 3), opposingPiece);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(5, 3));
+        });
+
+        it('cannot move diagonally if there is no piece to take', () => {
+            const pawn = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(4, 4), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(5, 3));
+        });
+
+        it('cannot take a friendly piece', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const friendlyPiece = new Rook(Player.WHITE);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(5, 3), friendlyPiece);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(5, 3));
+        });
+
+        it('cannot take the opposing king', () => {
+            const pawn = new Pawn(Player.WHITE);
+            const opposingKing = new King(Player.BLACK);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(5, 3), opposingKing);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(5, 3));
+        });
+
+>>>>>>> c0f07d1 (Added a test that pawns can take pieces)
     });
 
     describe('black pawns', () => {
@@ -61,6 +116,59 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(4, 7), Square.at(5, 7)]);
         });
 
+<<<<<<< HEAD
+=======
+        it('cannot move at the bottom of the board', () => {
+            const pawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(0, 3), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.be.empty;
+        });
+
+        it('can move diagonally if there is a piece to take', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const opposingPiece = new Rook(Player.WHITE);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(3, 3), opposingPiece);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.deep.include(Square.at(3, 3));
+        });
+
+        it('cannot move diagonally if there is no piece to take', () => {
+            const pawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(4, 4), pawn);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(3, 3));
+        });
+
+        it('cannot take a friendly piece', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const friendlyPiece = new Rook(Player.BLACK);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(3, 3), friendlyPiece);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(3, 3));
+        });
+
+        it('cannot take the opposing king', () => {
+            const pawn = new Pawn(Player.BLACK);
+            const opposingKing = new King(Player.WHITE);
+            board.setPiece(Square.at(4, 4), pawn);
+            board.setPiece(Square.at(3, 3), opposingKing);
+
+            const moves = pawn.getAvailableMoves(board);
+
+            moves.should.not.deep.include(Square.at(3, 3));
+        });
+>>>>>>> c0f07d1 (Added a test that pawns can take pieces)
     });
 
     it('cannot move if there is a piece in front', () => {
