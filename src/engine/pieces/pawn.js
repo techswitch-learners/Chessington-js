ea6326df0253b45d6dc3e39496e690dbdee35070
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:33:06 +0100

    can move one square if two square in front

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -6,56 +6,78 @@ export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-    getBlockPieceArr (board, squareAhead){
+
+    getBlackBlockingArr(board, location) {
+        let squareAhead = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
         let blockingPieceArr = [];
         for (let i = 0; i < 2; i++){
             if (board.getPiece(squareAhead[i])) {
                 blockingPieceArr.push(1); // 1 For not empty, 0 for empty
             } else {
                 blockingPieceArr.push(0)
+                }
             }
-        }
+        return blockingPieceArr;
+    }
+
+    getWhiteBlockingArr(board, location) {
+        let squareAhead = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+        let blockingPieceArr = [];
+        for (let i = 0; i < 2; i++){
+            if (board.getPiece(squareAhead[i])) {
+                blockingPieceArr.push(1); // 1 For not empty, 0 for empty
+            } else {
+                blockingPieceArr.push(0)
+                }
+            }
         return blockingPieceArr;
     }
 
     getAvailableMoves(board) {
         let location = board.findPiece(this) //Pawn : (6, 3)  blockPiece : (5, 3)
-        let squareAhead;
-        let blockingPieceArr;
         if(this.player === Player.BLACK) {
-            squareAhead = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
-            blockingPieceArr = getBlockPieceArr (board, squareAhead);
-        } else {
-            squareAhead = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
-            blockingPieceArr = getBlockPieceArr (board, squareAhead);
+            let blockingPieceArr = this.getBlackBlockingArr(board, location)
+            if(blockingPieceArr[0] === 1){
+                return [];
+            } else if (location.row === 6 && !blockingPieceArr.includes(1)){
+                return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+            } else if (location.row === 6 && blockingPieceArr[1] == 1){
+                return [Square.at(location.row - 1, location.col)]
+            } else if (location.row != 6 && blockingPieceArr[0] != 1) {
+                return [Square.at(location.row - 1, location.col)]
+            }
+        } else if (this.player === Player.WHITE){
+            let blockingPieceArr = this.getWhiteBlockingArr(board, location);
+                if(blockingPieceArr[0] === 1){
+                    return [];
+                } else if (location.row === 1 && !blockingPieceArr.includes(1)){
+                    return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+                } else if (location.row === 1 && blockingPieceArr[1] == 1){
+                    return [Square.at(location.row + 1, location.col)]
+                } else if (location.row != 1 && blockingPieceArr[0] != 1) {
+                    return [Square.at(location.row + 1, location.col)]
+                }
         }
 
-        // let blockingPieceArr = [];
-        // for (let i = 0; i < 2; i++){
-        //     if (board.getPiece(squareAhead[i])) {
-        //         blockingPieceArr.push(1); // 1 For not empty, 0 for empty
-        //     } else {
-        //         blockingPieceArr.push(0)
-        //     }
-        // }
+        
 
         // blockingPieceArr = [0, 1]
-        if (blockingPieceArr.includes(1) && blockingPieceArr[0] === 1){
-            return []; 
-        } else if(blockingPieceArr[0] === 0) {
-            return [Square.at(location.row - 1, location.col)];
-        }
-
-
-        if (this.player === Player.WHITE && location.row === 1) {
-            return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
-        } else if (this.player === Player.WHITE && location.row != 1) {
-            return [Square.at(location.row + 1, location.col)]
-        } else if (this.player === Player.BLACK && location.row === 6) {
-            return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
-        } else if (this.player === Player.BLACK && location.row != 6) {
-            return [Square.at(location.row - 1, location.col)]
-        }
+        // if (blockingPieceArr.includes(1) && blockingPieceArr[0] === 1){
+        //     return []; 
+        // } else if(blockingPieceArr[0] === 0) {
+        //     return [Square.at(location.row - 1, location.col)];
+        // }
+        
+            
+        // if (this.player === Player.WHITE && location.row === 1) {
+        //     return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+        // } else if (this.player === Player.WHITE && location.row != 1) {
+        //     return [Square.at(location.row + 1, location.col)]
+        // } else if (this.player === Player.BLACK && location.row === 6) {
+        //     return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+        // } else if (this.player === Player.BLACK && location.row != 6) {
+        //     return [Square.at(location.row - 1, location.col)]
+        // }
     }
-}
 
+}
--- tests/engine/pieces/pawn.spec.js
+++ tests/engine/pieces/pawn.spec.js
@@ -1,10 +1,6 @@
 import 'chai/register-should';
 import Pawn from '../../../src/engine/pieces/pawn';
 import Rook from '../../../src/engine/pieces/rook';
-<<<<<<< HEAD
-import King from '../../../src/engine/pieces/king';
-=======
->>>>>>> 55d8468 (Added a test that pawns cannot move through other pieces)
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
@@ -37,57 +33,6 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(2, 7), Square.at(3, 7)]);
         });
 
-        it('cannot move at the top of the board', () => {
-            const pawn = new Pawn(Player.WHITE);
-            board.setPiece(Square.at(7, 3), pawn);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.be.empty;
-        });
-
-        it('can move diagonally if there is a piece to take', () => {
-            const pawn = new Pawn(Player.WHITE);
-            const opposingPiece = new Rook(Player.BLACK);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(5, 3), opposingPiece);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.deep.include(Square.at(5, 3));
-        });
-
-        it('cannot move diagonally if there is no piece to take', () => {
-            const pawn = new Pawn(Player.WHITE);
-            board.setPiece(Square.at(4, 4), pawn);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(5, 3));
-        });
-
-        it('cannot take a friendly piece', () => {
-            const pawn = new Pawn(Player.WHITE);
-            const friendlyPiece = new Rook(Player.WHITE);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(5, 3), friendlyPiece);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(5, 3));
-        });
-
-        it('cannot take the opposing king', () => {
-            const pawn = new Pawn(Player.WHITE);
-            const opposingKing = new King(Player.BLACK);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(5, 3), opposingKing);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(5, 3));
-        });
-
     });
 
     describe('black pawns', () => {
@@ -116,57 +61,6 @@ describe('Pawn', () => {
             moves.should.deep.include.members([Square.at(4, 7), Square.at(5, 7)]);
         });
 
-
-        it('cannot move at the bottom of the board', () => {
-            const pawn = new Pawn(Player.BLACK);
-            board.setPiece(Square.at(0, 3), pawn);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.be.empty;
-        });
-
-        it('can move diagonally if there is a piece to take', () => {
-            const pawn = new Pawn(Player.BLACK);
-            const opposingPiece = new Rook(Player.WHITE);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(3, 3), opposingPiece);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.deep.include(Square.at(3, 3));
-        });
-
-        it('cannot move diagonally if there is no piece to take', () => {
-            const pawn = new Pawn(Player.BLACK);
-            board.setPiece(Square.at(4, 4), pawn);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(3, 3));
-        });
-
-        it('cannot take a friendly piece', () => {
-            const pawn = new Pawn(Player.BLACK);
-            const friendlyPiece = new Rook(Player.BLACK);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(3, 3), friendlyPiece);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(3, 3));
-        });
-
-        it('cannot take the opposing king', () => {
-            const pawn = new Pawn(Player.BLACK);
-            const opposingKing = new King(Player.WHITE);
-            board.setPiece(Square.at(4, 4), pawn);
-            board.setPiece(Square.at(3, 3), opposingKing);
-
-            const moves = pawn.getAvailableMoves(board);
-
-            moves.should.not.deep.include(Square.at(3, 3));
-        });
     });
 
     it('cannot move if there is a piece in front', () => {
@@ -201,28 +95,6 @@ describe('Pawn', () => {
         const moves = pawn.getAvailableMoves(board);
 
         moves.should.deep.include(Square.at(5, 3));
-    });
-
-    it('cannot move if there is a piece in front', () => {
-        const pawn = new Pawn(Player.BLACK);
-        const blockingPiece = new Rook(Player.WHITE);
-        board.setPiece(Square.at(6, 3), pawn);
-        board.setPiece(Square.at(5, 3), blockingPiece);
-
-        const moves = pawn.getAvailableMoves(board);
-
-        moves.should.be.empty;
-    });
-
-    it('cannot move two squares if there is a piece two sqaures in front', () => {
-        const pawn = new Pawn(Player.BLACK);
-        const blockingPiece = new Rook(Player.WHITE);
-        board.setPiece(Square.at(6, 3), pawn);
-        board.setPiece(Square.at(4, 3), blockingPiece);
-
-        const moves = pawn.getAvailableMoves(board);
-
-        moves.should.not.deep.include(Square.at(4, 3));
     });
 
 });
