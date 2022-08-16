611502bae593023b274086756d428734f4e2accc
Leo Ng <leo.ng@softwire.com>
2022-08-16 16:46:22 +0100

    made pawns move 1 or 2 step

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -9,10 +9,14 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+        if (this.player === Player.WHITE && location.row === 1) {
+            return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)]
+        } else if (this.player === Player.WHITE && location.row != 1) {
+            return [Square.at(location.row + 1, location.col)]
+        } else if (this.player === Player.BLACK && location.row === 6) {
+            return [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)]
+        } else if (this.player === Player.BLACK && location.row != 6) {
+            return [Square.at(location.row - 1, location.col)]
         }
     }
 }
